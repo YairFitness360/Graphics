0e409f2c0c2a4f2f6415be1e6e2ffd273d6f7e6c
yuval hoch <hochyuv@macs.biu.ac.il>
2019-04-22 14:57:48 +0300

    main fixed

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,6 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        Transform test = new Transform();
-        double[][] matrix = test.Translate(1, 2, 3);
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-
         MyCanvas canvas = new MyCanvas();
         JFrame frame = new JFrame();
         frame.setSize(800 + 40,800 + 40);
